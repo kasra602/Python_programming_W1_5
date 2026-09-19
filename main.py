74bb779025e702d8560365a7aa11fd3b43df654c
Kasra zahedi <106477508+kasra602@users.noreply.github.com>
2026-09-19 22:24:37 +0300

    Fix print statement formatting for width and height

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ print ("Calculate the area of a wall.")
 Width = int(input("Enter the Width in meters: "))   
 Height = int(input("Enter the Height in meters: "))
 Area = Width * Height
-print("Width is:", Width,"m and Height is:", Height,"m")
+print("Width is", Width,"m and Height is", Height,"m")
 print("The wall will be ", Area, "square meters.")
