c8630e6c74ab80fb3dcc8f447000185ab43db209
Kasra zahedi <106477508+kasra602@users.noreply.github.com>
2026-09-19 22:23:58 +0300

    Update input prompts and output messages for consistency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print ("Calculate the area of a wall.")
-Width = int(input("Enter the width in meters: "))   
-Height = int(input("Enter the height in meters: "))
+Width = int(input("Enter the Width in meters: "))   
+Height = int(input("Enter the Height in meters: "))
 Area = Width * Height
-print("Width is:", Width,"m and height is:", Height,"m")
+print("Width is:", Width,"m and Height is:", Height,"m")
 print("The wall will be ", Area, "square meters.")
